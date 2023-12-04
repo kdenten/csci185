ea69c4ec7e7dbb0dd4ce6b08e86a627ebb65981d
Katie <katden1738m@gmail.com>
2023-12-04 16:21:37 -0500

    12/4

---
+++ quizzes/quiz03/main.js
@@ -0,0 +1,23 @@
+function color1() {
+    document.querySelector("#square1").style.backgroundColor = "pink";
+}
+
+function color2() {
+    document.querySelector("#square2").style.backgroundColor = "teal";
+}
+
+function color3() {
+    document.querySelector("#square3").style.backgroundColor = "lavender";
+}
+
+function color4() {
+    document.querySelector("#square4").style.backgroundColor = "magenta";
+}
+
+function color5() {
+    document.querySelector("#square5").style.backgroundColor = "darkgreen";
+}
+
+function color6() {
+    document.querySelector("#square6").style.backgroundColor = "red";
+}
